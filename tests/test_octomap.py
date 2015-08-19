6c433c7f5b069c176d895dfe4ca6518f438398c9
mklingen <theotherguy1989@gmail.com>
2015-08-19 17:23:59 -0400

    fixing more merge problems

--- tests/test_octomap.py
+++ tests/test_octomap.py
@@ -10,7 +10,7 @@ objects_path = find_in_workspaces(
     path='data/objects',
     first_match_only=True)[0]
 
-env, robot = herbpy.initialize(sim=False, attach_viewer='rviz')
+env, robot = herbpy.initialize(sim=True, attach_viewer='rviz')
 table_file = os.path.join(objects_path, 'table.kinbody.xml')
 table = env.ReadKinBodyXMLFile(table_file)
 env.AddKinBody(table)
@@ -42,7 +42,7 @@ env.SetCollisionChecker(collision_checker)
 '''
 
 
-raw_input('press enter to begin end planning')
+#raw_input('press enter to begin end planning')
 #plan to some pose
-end_config = np.array([2.336375162301677, -0.7521315673852575, 2.6228803736881265, 0.5508421753993923, -0.01587398287783462, -0.2861305355919238, -0.7392284125047297])
-robot.right_arm.PlanToConfiguration(end_config,execute=True)
+#end_config = np.array([2.336375162301677, -0.7521315673852575, 2.6228803736881265, 0.5508421753993923, -0.01587398287783462, -0.2861305355919238, -0.7392284125047297])
+#robot.right_arm.PlanToConfiguration(end_config,execute=True)
